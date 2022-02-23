23c0f60a15f4544722533431989f302270f55786
Tiandra Lall <70166377+tiandralall@users.noreply.github.com>
2022-02-23 20:59:33 +0000

    assign2

--- assign3/task2/convert_2.c
+++ assign3/task2/convert_2.c
@@ -1,19 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
-//Program to convert a 32-bit Decimal numbers to Binary
-//1) Do not change the signature of convert_2, or your task receives zero
-//2) No globals allowed.
-char* convert_2(int dec)
-{
-	//TODO: your implementation
-}
+
+char* convert_2(int dec){
+ char* ans = (char* )malloc(50);
+  int mask = 0x80000000;
+  int i=0,j;
+  ans[i++] = '0';
+  ans[i++] = 'b';
+
+  for(j=0;j<32;j++) {
+      if(j%4 == 0 && j > 0) ans[i++] = ' ';
+     
+      if(dec & mask) ans[i++] = '1';
+      else ans[i++] = '0';
+      dec <<= 1;
+  }
+  return ans;}
 
 int main() {
 	int n;
 	char * bin;
-	printf("Enter the Decimal Number\n");
+	printf("Enter the Decimal Number below \n");
 	scanf("%d",&n);
 	bin = convert_2(n);
 	printf("The Binary Notation of %d is\t %s\n", n, bin);
-	//TODO: do we need to release the memory of bin?
+  free(bin);
 }
+}
+
--- assign3/task5/set_bits.c
+++ assign3/task5/set_bits.c
@@ -1,16 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-// Set the bits of x within range of [start, end], in which both are inclusive
-// Assume 0 <= start & end <= 31
-void set_bits(unsigned * x,
-             unsigned start,
-             unsigned end,
-             unsigned *v) {
-    // YOUR CODE HERE
-    // No return value
-    // v points to an array of at least (end-start+1) unsigned integers.
-    // if v[i] == 0, then set (i+start)-th bit of x zero, otherwise, set (i+start)-th bit of x one.
+char* convert_2(int dec){
+ char* ans = (char* )malloc(50);
+  int mask = 0x80000000;
+  int i=0,j;
+  ans[i++] = '0';
+  ans[i++] = 'b';
+
+  for(j=0;j<32;j++) {
+      if(j%4 == 0 && j > 0) ans[i++] = ' ';
+     
+      if(dec & mask) ans[i++] = '1';
+      else ans[i++] = '0';
+      dec <<= 1;
+  }
+  return ans;}
+
+int main() {
+	int n;
+	char * bin;
+	printf("Enter the Decimal Number below \n");
+	scanf("%d",&n);
+	bin = convert_2(n);
+	printf("The Binary Notation of %d is\t %s\n", n, bin);
+  free(bin);
 }
+}
+
 
 
